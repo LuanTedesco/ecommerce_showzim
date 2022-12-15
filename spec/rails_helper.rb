a6220b285b6ede5f30b4555f7e8744877541addb
Luan Tedesco <luanfelipe2040@gmail.com>
2022-12-15 10:28:17 -0300

    Configurando faker

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,12 +2,13 @@
 require 'spec_helper'
 require 'vcr'
 require 'simplecov'
+require 'faker'
 
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
