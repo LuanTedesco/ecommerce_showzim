6679c5472c778812c59ca11f80a3ee5319ae85a5
Luan Tedesco <luanfelipe2040@gmail.com>
2023-01-18 15:08:36 -0300

    Instalando o layout administrativo

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'home#index'
+
+  namespace :admin do
+    root to: 'home#index'
+  end
 end
