c6d18e93dd28a38c74bfd79eceb3ba9e9280a79e
Luan Tedesco <luanfelipe2040@gmail.com>
2023-01-23 15:44:42 -0300

    Crud de produto

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,56 @@
+module Admin
+  class ProductsController < BaseController
+    before_action :set_product, only: %i[show edit update destroy]
+
+    def index
+      @products = Product.all
+    end
+
+    def edit; end
+
+    def new
+      @product = Product.new
+    end
+
+    def show; end
+
+    def create
+      @product = Product.new(product_params)
+      respond_to do |format|
+        if @product.save
+          format.html { redirect_to admin_products_path(@product), notice: 'Produto cadastrado com sucesso' }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def update
+      @product = Product.new(product_params)
+      respond_to do |format|
+        if @product.update
+          format.html { redirect_to admin_products_path(@product), notice: 'Produto cadastrado com sucesso' }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @product.destroy
+      respond_to do |format|
+        format.html { redirect_to admin_products_path, notice: 'Produto excluido com sucesso' }
+      end
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :publish, :category_id)
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+  end
+end
