e87e93c1dcdb67ccc831c9207126004f02e18aa0
Luan Tedesco <luanfelipe2040@gmail.com>
2023-02-08 15:22:58 -0300

    Atualizando os dados do usuário

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,9 +1,18 @@
 class SettingsController < ApplicationController
   before_action :authenticate_user!
+
   def index
   end
 
-  def update
-    
+  def update_user
+    if current_user.update(user_params)
+      redirect_to settings_path, notice: 'Usuário atualizado com sucesso.'
+    else
+      render :index
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :phone)
   end
 end
